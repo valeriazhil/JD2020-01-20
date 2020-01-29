10d9fa23832f0256566578b6053dbc08fe31549a
cherkas <andrey.cherkas@gmail.com>
2020-01-29 23:36:57 +0300

    test

---
+++ src/by/it/cherkas/jd01_05/TaskB.java
@@ -0,0 +1,20 @@
+package by.it.cherkas.jd01_05;
+
+import static java.lang.Math.*;
+
+public class TaskB {
+    public static void main(String[] args) {
+        double y = 0;
+
+        double x = 0;
+        int a = 0;
+        for (int i = 0; i < 6; i ++) {
+
+
+            y +=i;
+
+            System.out.println(y);
+
+        }
+    }
+}
