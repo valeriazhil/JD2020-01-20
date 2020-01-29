6ac5c57842c20bd63d8515de070f10d7314e8c22
cherkas <andrey.cherkas@gmail.com>
2020-01-29 21:16:22 +0300

    test

---
+++ src/by/it/cherkas/jd01_01/TaskA2.java
@@ -0,0 +1,21 @@
+package by.it.cherkas.jd01_01;
+
+/*
+напишите тело main программы которая выводит на экран 5 строк
+"Я начинаю изучать Java!"
+
+Для ручной проверки запустите программу Ctrl+Shift+F10
+Для автоматической проверки откройте и запустите класс Testing
+*/
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+class TaskA2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Я начинаю изучать Java!");
+
+        }
+    }
+
+}
---
+++ src/by/it/cherkas/jd01_03/Runner.java
@@ -0,0 +1,17 @@
+package by.it.cherkas.jd01_03;
+
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        double[] array = InOut.getArray(str);
+        InOut.printArray(array);
+        InOut.printArray(array, "V", 3);
+        System.out.println("Минимум массива min=" + Helper.findMin(array));
+        System.out.println("Максимум массива max=" + Helper.findMax(array));
+        System.out.println("Сортировка: ");
+        Helper.sort(array);
+    }
+}
