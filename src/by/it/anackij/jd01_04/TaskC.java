26a3cfc0d5544aa93267cb373ffb30807914a84f
naglec13 <naglec13@gmail.com>
2020-01-30 21:53:50 +0300

    solve A,B1

--- src/by/it/anackij/jd01_04/TaskC.java
+++ src/by/it/anackij/jd01_04/TaskC.java
@@ -6,8 +6,10 @@ public class TaskC {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
-        buildOneDimArray(line);
+        double[] array = buildArray(line);
+        mergeSort(array);
     }
+
     static void buildOneDimArray(String line) {
         double[] array = InOut.getArray(line);
         InOut.printArray(array, "V", 5);
@@ -29,14 +31,28 @@ public class TaskC {
 
     }
 
-    static void mergeSort(int[] array){
-        int left=0;
-        int right=array.length;
-        int medium=array.length/2;
-        for (int i = 0; i < array.length; i++) {
-            while (left<=right){
-                medium = (left+right)/2;
-            }
+    static double[] buildArray(String line) {
+        double[] array = InOut.getArray(line);
+        InOut.printArray(array, "V", 5);
+        double first = array[0];
+        double last = array[array.length - 1];
+
+        System.out.println();
+        return array;
+    }
+
+    static void mergeSort(double[] array) {
+        int logo = (int) (Math.log(array.length) / Math.log(2.0));
+        int[] indexAr = new int[logo];
+        int left = 0;
+        int right = array.length - 1;
+        int medium = 1;
+        for (int i = 0; i < logo; i++) {
+            medium = (left + right) / 2;
+            indexAr[i] = medium;
+            System.out.printf("%s%d/%s%d/%s%d%n", "left= ", left, "medium= ", medium, "right= ", right);
+            right = medium;
         }
     }
 }
+
