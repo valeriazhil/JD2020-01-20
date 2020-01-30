700bee14d3316f9559d8060048a7c4fd57f337fe
Degtyaryov Eugene <EDegtyaryov@qulix.com>
2020-01-30 08:37:16 +0300

    Resolve TaskC

---
+++ src/by/it/degtyaryov/jd01_05/InOut.java
@@ -0,0 +1,27 @@
+package by.it.degtyaryov.jd01_05;
+
+/**
+ * Класс для ввода-вывода массивов
+ *
+ * @author DEA
+ */
+class InOut {
+
+    /**
+     * Выводит в консоль значения массива в заданном формате
+     *
+     * @param array       одномерный массив типа double
+     * @param arrayName   имя массива
+     * @param columnCount количество столбцов для вывода
+     * @author DEA
+     */
+    static void printArray(double[] array, String arrayName, int columnCount) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%s[% -3d] = %-8.5f ", arrayName, i, array[i]);
+            if ((i + 1) % columnCount == 0 || i == array.length - 1) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+}
