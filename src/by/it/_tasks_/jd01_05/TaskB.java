1044c059ad60250548bf3ca41a1fa1568240e756
Scaryshki <Scaryshki@gmail.com>
2020-01-31 11:45:23 +0300

    It,s me

--- src/by/it/_tasks_/jd01_05/TaskB.java
+++ src/by/it/_tasks_/jd01_05/TaskB.java
@@ -5,11 +5,12 @@ public class TaskB {
     public static void main(String[] args) {
         TaskB2();
 
-        for (double a = 0; a <=2 ; a=a+0.20) {
-            for (int x = 1; x <=6; x++) {
 
-                double y =x*Math.pow(7,a)-x*Math.cos(x);
-                System.out.printf("При a=%.2f Сумма y =%f%n",a, y);
+        for (double a = 0; a <=2 ; a=a+0.20) {
+            double sum = 0;
+            for (int x = 1; x <=6; x++) {
+                sum+= Math.pow(7,a) - Math.cos(x);
+                System.out.printf("При a=%.2f Сумма y =%f%n",a, sum);
             }
         }
     }
