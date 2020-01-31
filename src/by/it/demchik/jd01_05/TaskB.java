b9bf883392ae49f6ccbe518d01a57c613ff63601
vadimdemchik <demchikvadimm@gmail.com>
2020-01-31 09:52:04 +0300

    commit

--- src/by/it/demchik/jd01_05/TaskB.java
+++ src/by/it/demchik/jd01_05/TaskB.java
@@ -1,18 +1,33 @@
 package by.it.demchik.jd01_05;
 
-import static java.lang.Math.*;
+import static java.lang.Math.cos;
+import static java.lang.Math.pow;
 
 public class TaskB {
     public static void main(String[] args) {
-        int x = 1;
-        int i =6;
-        for (double a = 0; a <= 2.0; a = a + 0.2) {
-            double y =((pow(7,a))-cos(x));
-            Su
+        step1();
+        step2();
 
-            System.out.printf("При а=%6.2f y=%g\n", a, y);
+    }
+
+    private static void step1() {
+
+        double sum = 0;
+
+        for (double a = 0; a <= 2.0; a = a + 0.2) {
+
+            for (int x = 1; x <= 6; x++) {
+
+
+                sum = sum + ((pow(7, a)) - cos(x));
+                System.out.printf("При а=%6.2f y=%g\n", a, sum);
+            }
+
         }
 
     }
 
+    private static void step2() {
+
     }
+}
