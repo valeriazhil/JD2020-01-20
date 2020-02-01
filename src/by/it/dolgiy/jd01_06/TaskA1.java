d446a45781400c0172e2eda47cdf9210c8c16b0a
EvgeniyD1 <edolgiyj7@gmail.com>
2020-02-01 11:49:12 +0300

    111

---
+++ src/by/it/dolgiy/jd01_06/TaskA1.java
@@ -0,0 +1,9 @@
+package by.it.dolgiy.jd01_06;
+
+public class TaskA1 {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder(Poem.text);
+        
+        System.out.println(Poem.text);
+    }
+}
