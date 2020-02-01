e0f63917ecb5225ebccc6c9dcdd228c99f7dcc5a
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-02-02 00:53:19 +0300

    Completed C2! :)

--- src/by/it/zhuravaskarabahataya/jd01_06/TaskA1.java
+++ src/by/it/zhuravaskarabahataya/jd01_06/TaskA1.java
@@ -2,7 +2,7 @@ package by.it.zhuravaskarabahataya.jd01_06;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+//А что, matcher одноразовый???
 public class TaskA1 {
     public static void main(String[] args) {
         StringBuilder text = new StringBuilder(Poem.text);
