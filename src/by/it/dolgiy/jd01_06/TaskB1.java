2e0c219acb288ed49e361b861f7272740d9c626a
EvgeniyD1 <edolgiyj7@gmail.com>
2020-02-02 20:05:04 +0300

    jd01_06: A, B/2

---
+++ src/by/it/dolgiy/jd01_06/TaskB1.java
@@ -0,0 +1,36 @@
+package by.it.dolgiy.jd01_06;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+
+    private static char[] gl = {'а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е', 'А', 'У', 'О', 'Ы', 'И', 'Э', 'Я',
+            'Ю', 'Ё', 'Е'};
+    private static char[] sog = {'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф',
+            'х', 'ц', 'ч', 'ш', 'щ'};
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()){
+            String word = matcher.group();
+            char[] w = word.toCharArray();
+            char start = w[0];
+            char end = w[w.length-1];
+            for (int j = 0; j <= sog.length - 1; j++) {
+                if (start == end) {
+                    break;
+                    }
+                if (start == sog[j]) {
+                    for (int k = 0; k <= gl.length - 1; k++) {
+                        if (end == gl[k]) {
+                                System.out.println(word);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
