10058e684e2cb82ec385494331028048e07e77d5
zhilenkova <valeriz.zhil@gmail.com>
2020-02-03 10:27:30 +0300

    one

--- src/by/it/zhilenkova/jd01/jd01_06/TaskA1.java
+++ src/by/it/zhilenkova/jd01/jd01_06/TaskA1.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskA1 {
-
+//hi
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder(Poem.text);
         Pattern pattern = Pattern.compile("[а-яА-яёЁ]{4,}");
