c4b82ff144112c1591c780f3d5033612ca4304c8
naglec13 <naglec13@gmail.com>
2020-02-04 01:42:02 +0300

    ..

--- src/by/it/anackij/jd01_06/TaskA2.java
+++ src/by/it/anackij/jd01_06/TaskA2.java
@@ -5,8 +5,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskA2 {
-    private static String[] words = {};
-    private static int[] counters = {};
+    private static String[] words = new String[0];
+    private static int[] counters = new int[0];
 
     public static void main(String[] args) {
         Pattern patternWords = Pattern.compile("[а-яА-ЯёЁ]+");
@@ -21,16 +21,15 @@ public class TaskA2 {
     }
 
     private static void process(String word) {
-        for (int i = 0; i < word.length(); i++) {
-            if (words[i].equals(word)) {
+        for (int i = 0; i < words.length; i++) {
+            if (word.equals(words[i])) {
                 counters[i]++;
                 return;
-            } else {
-                words = Arrays.copyOf(words, words.length + 1);
-                counters = Arrays.copyOf(counters, counters.length + 1);
-                words[words.length - 1] = word;
-                counters[counters.length - 1] = 1;
             }
         }
+        words = Arrays.copyOf(words, words.length + 1);
+        counters = Arrays.copyOf(counters, counters.length + 1);
+        words[words.length - 1] = word;
+        counters[counters.length - 1] = 1;
     }
 }
