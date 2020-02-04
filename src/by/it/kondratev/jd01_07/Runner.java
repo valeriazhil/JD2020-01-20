bb3a6c4ced762700703133bd83c240ab48e811ce
Михаил <potapy4@tut.by>
2020-02-04 20:23:52 +0300

    01-07 B3

--- src/by/it/kondratev/jd01_07/Runner.java
+++ src/by/it/kondratev/jd01_07/Runner.java
@@ -1,11 +1,29 @@
 package by.it.kondratev.jd01_07;
 
+import static java.lang.Double.parseDouble;
+
 public class Runner {
 
     public static void main(String[] args) {
-        Var v1 = new  Scalar(3.1415);
-        Var v2 = new Vector(new double[]{1, 2, 4});
-        System.out.println(v1);
+        //Var v1 = new  Scalar(3.1415);
+        Var v2 = new Vector(new double[]{1,2,4});
+        //Var v3 = new Matrix(new double[][]{{1, 2}, {3, 4}});
+        //System.out.println(v1);
         System.out.println(v2);
+        //System.out.println(v3);
+
+        /*String strVector = "{1,2,4}";
+        strVector = strVector.substring(1, strVector.length() - 1);
+        //    strVector.(',','n');
+        System.out.println(strVector);
+        //strVector.replace('}',' ');
+        //strVector = strVector.trim();
+        String[] arr = strVector.split(",");
+        double [] mas = new double[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            mas[i]=Double.parseDouble(arr[i]);
+            System.out.println(arr[i]+"==="+ mas[i]);
+        }*/
     }
 }
+
