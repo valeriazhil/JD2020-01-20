5954b78ffcf2bf1bbf7160206cf2db512335d5a1
Михаил <potapy4@tut.by>
2020-02-04 21:17:36 +0300

    01-07 C3

--- src/by/it/kondratev/jd01_07/Runner.java
+++ src/by/it/kondratev/jd01_07/Runner.java
@@ -6,20 +6,34 @@ public class Runner {
 
     public static void main(String[] args) {
         //Var v1 = new  Scalar(3.1415);
-        Var v2 = new Vector(new double[]{1,2,4});
+        //Var v2 = new Vector(new double[]{1,2,4});
         //Var v3 = new Matrix(new double[][]{{1, 2}, {3, 4}});
         //System.out.println(v1);
-        System.out.println(v2);
+        //System.out.println(v2);
         //System.out.println(v3);
 
-        /*String strVector = "{1,2,4}";
-        strVector = strVector.substring(1, strVector.length() - 1);
+        String strVector = "{{1,2,5},{3,4,8},{5,6,9},{7,8,1}}";
+        strVector = strVector.substring(1, strVector.length() - 2);
         //    strVector.(',','n');
-        System.out.println(strVector);
         //strVector.replace('}',' ');
         //strVector = strVector.trim();
-        String[] arr = strVector.split(",");
-        double [] mas = new double[arr.length];
+        String[] row = strVector.split("},");
+        for (int i = 0; i <row.length ; i++) {
+            row[i] = row[i].substring(1);
+        }
+        String [] cols = row[0].split(",");
+        double [][] mas = new double[row.length][cols.length];
+        for (int i = 0; i < row.length ; i++) {
+            String [] col = row[i].split(",");
+            for (int j = 0; j < col.length ; j++) {
+                mas [i][j] = Double.parseDouble(col[j]);
+                System.out.print(mas [i][j]+" ");
+            }
+            System.out.println();
+
+
+        }
+        /*double [] mas = new double[arr.length];
         for (int i = 0; i < arr.length; i++) {
             mas[i]=Double.parseDouble(arr[i]);
             System.out.println(arr[i]+"==="+ mas[i]);
