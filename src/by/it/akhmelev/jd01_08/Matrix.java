d54f1f1f1a638417cdd3d28367e8b24848202996
loktev <loktev@tut.by>
2020-02-05 12:52:49 +0300

    classwork lesson 01_08 TaskA

---
+++ src/by/it/akhmelev/jd01_08/Matrix.java
@@ -0,0 +1,13 @@
+package by.it.akhmelev.jd01_08;
+
+public class Matrix extends Var {
+
+    public Matrix(String s) {
+        //
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix";
+    }
+}
