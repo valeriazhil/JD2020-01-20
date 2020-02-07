9f2e7c18295f0de0e26452f72dd3852c5b4d07ea
Pavel <pashakochybey@mail.ru>
2020-02-07 13:22:07 +0200

    Task01_06-01_08

--- src/by/it/pkochubei/jd01_03/Runner.java
+++ src/by/it/pkochubei/jd01_03/Runner.java
@@ -1,13 +1,13 @@
 package by.it.pkochubei.jd01_03;
 
 import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         double[] array = InOut.getArray(str);
         InOut.printArray(array);
-        InOut.printArray(array,"M",4);
-        //
+        InOut.printArray(array, "M", 4);
     }
 }
