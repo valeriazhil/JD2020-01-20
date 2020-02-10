2cfdbba8762a0a267425bb1259d787d1ea241aad
naglec13 <naglec13@gmail.com>
2020-02-10 11:39:00 +0300

    ..

--- src/by/it/anackij/jd01_08/Vector.java
+++ src/by/it/anackij/jd01_08/Vector.java
@@ -4,19 +4,19 @@ package by.it.anackij.jd01_08;
 import java.util.Arrays;
 
 public class Vector extends Var {
-    private double[] value;
+    private double[] values;
     private double doubleValue;
     private String strValue;
 
     public Vector(double[] values) {
-        this.value = values;
+        this.values = values;
     }
 
 
 
     @Override
     public Var add(Var other) {
-        double[] result = Arrays.copyOf(value, value.length);
+        double[] result = Arrays.copyOf(values, values.length);
         if (other instanceof Scalar) {
             double s = ((Scalar) other).getValue();
             for (int i = 0; i < result.length; i++) {
@@ -25,7 +25,7 @@ public class Vector extends Var {
             }
         } else if (other instanceof Vector) {
             for (int i = 0; i < result.length; i++) {
-                result[i] = result[i] + new Vector((Vector) other).value[i];
+                result[i] = result[i] + new Vector((Vector) other).values[i];
                 return new Vector(result);
             }
         }
@@ -48,11 +48,17 @@ public class Vector extends Var {
     }
 
     public Vector(Vector other) {
-        this.value = other.value;
+        this.values = other.values;
     }
 
     public Vector(String strValue) {
-        this.doubleValue = Double.parseDouble(strValue);
+        String[] array;
+        array = strValue.trim().replace("{","").replace("}","").split(",");
+
+        values = new double[array.length];
+        for (int i = 0; i < array.length; i++) {
+            values[i] = Double.parseDouble(array[i]);
+        }
     }
 
     @Override
@@ -60,7 +66,7 @@ public class Vector extends Var {
         StringBuilder out = new StringBuilder();
         out.append('{');
         String delimiter = "";
-        for (double value : value) {
+        for (double value : values) {
             out.append(delimiter).append(value);
             delimiter = ", ";
         }
