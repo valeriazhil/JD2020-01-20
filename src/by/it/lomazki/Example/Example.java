e4817f2b13478f7bb176e5e1b88c917533facce1
Lomazki <sedovlasovich@yandex.by>
2020-02-11 12:53:34 +0300

    09.02.2020

--- src/by/it/lomazki/Example/Example.java
+++ src/by/it/lomazki/Example/Example.java
@@ -1,20 +1,39 @@
 package by.it.lomazki.Example;
 
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(numberToWords(number));
+    }
 
-        String input = "Hello Java! Hello JavaScript! JavaSE 8.";
+    public static String numberToWords(int number) {
+        String[] ones = // 0 .. 19
+                {
+                        "zero", "one", "two", "three", "four",
+                        "five", "six", "seven", "eight", "nine",
+                        "ten", "eleven", "twelve", "thirteen", "fourteen",
+                        "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+                };
 
-        Pattern pattern = Pattern.compile("Java(\\w*)");
-        Matcher matcher = pattern.matcher(input);
-        while(matcher.find())
-            System.out.println(matcher.group());
+        String[] tens = // 20 .. 100
+                {
+                        "twenty", "thirty", "forty", "fifty", "sixty",
+                        "seventy", "eighty", "ninety", "hundred"
+                };
 
+        int dec = number / 10 - 2;
+        int ed = number % 10;
 
+        if (number < 20)
+            return ones[number];
+        if (number % 10 == 0)
+            return tens[dec];
+        else
+            return tens[dec] + ones[ed];
     }
 }
 
