16e257c6d015f19ac61447421ef7bbb80e994eb4
Scaryshki <Scaryshki@gmail.com>
2020-02-12 12:23:02 +0300

    task 11

---
+++ src/by/it/makarenko/jd01_11/Runner.java
@@ -0,0 +1,31 @@
+package by.it.makarenko.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("2");
+        list.add("6");
+        list.add("lol");
+        list.add("one2");
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        System.out.println(list.size());
+
+        list = new ListA<>();
+        list.add("one");
+        list.add("2");
+        list.add("6");
+        list.add("lol");
+        list.add("one2");
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.get(3));
+    }
+}
