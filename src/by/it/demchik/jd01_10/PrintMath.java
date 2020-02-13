3ffaecb329258cc7d0d6056a913f440f0b97b25b
vadimdemchik <demchikvadimm@gmail.com>
2020-02-13 15:55:58 +0300

    comm 10

---
+++ src/by/it/demchik/jd01_10/PrintMath.java
@@ -0,0 +1,21 @@
+package by.it.demchik.jd01_10;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class PrintMath {
+    public static void main(String[] args) {
+       Class<Math>structMath=Math.class;
+    Method[]methods=structMath.getDeclaredMethods();
+        for (Method method : methods) {
+            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) ;
+
+
+            System.out.println(method);
+
+
+        }
+
+
+}}
+
