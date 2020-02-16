7a85cd92957e9867111c7354acf0714f1d9ca463
vadimdemchik <demchikvadimm@gmail.com>
2020-02-16 15:00:35 +0300

    comm 10

--- src/by/it/demchik/jd01_10/PrintMath.java
+++ src/by/it/demchik/jd01_10/PrintMath.java
@@ -1,21 +1,24 @@
 package by.it.demchik.jd01_10;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class PrintMath {
     public static void main(String[] args) {
-       Class<Math>structMath=Math.class;
-    Method[]methods=structMath.getDeclaredMethods();
+        Class<Math> structMath = Math.class;
+        Method[] methods = structMath.getDeclaredMethods();
         for (Method method : methods) {
-            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) ;
-
-
-            System.out.println(method);
-
-
+            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
+                System.out.println(method.toString().replace("java.lang.Math.", ""));
+        }
+        Field[] priv=structMath.getDeclaredFields();
+        for (Field field:priv) {
+            if((field.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
+            System.out.println(field.toString().replace("java.lang.Math.", ""));
         }
 
-
-}}
+        }
+}
 
---
+++ src/by/it/demchik/jd01_10/PrintString.java
@@ -0,0 +1,13 @@
+package by.it.demchik.jd01_10;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class PrintString {
+    public static void main(String[] args) {
+        Class<String> sys=String.class;
+
+
+        System.out.println(method.toString().replace("java.lang.String.",""));
+    }}
+}
