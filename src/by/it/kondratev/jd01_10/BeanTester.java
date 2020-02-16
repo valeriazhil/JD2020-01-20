734573fd23c396266057c35b83bf88ce1eed5d74
Михаил <potapy4@tut.by>
2020-02-16 22:41:01 +0300

    01-10 C1

--- src/by/it/kondratev/jd01_10/BeanTester.java
+++ src/by/it/kondratev/jd01_10/BeanTester.java
@@ -1,17 +1,22 @@
 package by.it.kondratev.jd01_10;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class BeanTester {
     public static void main(String[] args) throws Exception{
         Class<Bean> beanClass = Bean.class;
-        Method[] methods = beanClass.getDeclaredMethods();
         Object o = beanClass.newInstance();
+        Method[] methods = beanClass.getDeclaredMethods();
         for (Method method : methods) {
-            if (method.getDeclaredAnnotations()!=null) {
-                method.invoke(method.getName());   // ????
-                System.out.println(method.getName());
-                System.out.println(method);
+            if (method.isAnnotationPresent(Param.class)) {
+                Param annotation = method.getDeclaredAnnotation(Param.class);
+                int a = annotation.a();
+                int b = annotation.b();
+                Object result = null;
+                if (Modifier.isStatic(method.getModifiers()) )
+                    System.out.println(method.getName() +"   result="+method.invoke(null,a,b));
+                else  System.out.println(method.getName() +"   result="+method.invoke(o,a,b));
             }
         }
     }
