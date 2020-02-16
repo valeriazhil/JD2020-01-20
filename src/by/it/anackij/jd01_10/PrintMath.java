34561f1e47e0889819b5526f81e0d26f2f07d320
naglec13 <naglec13@gmail.com>
2020-02-16 22:48:31 +0300

    jd01_10 B1

--- src/by/it/anackij/jd01_10/PrintMath.java
+++ src/by/it/anackij/jd01_10/PrintMath.java
@@ -1,5 +1,6 @@
 package by.it.anackij.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
@@ -12,16 +13,29 @@ public class PrintMath {
             StringBuilder stringBuilder = new StringBuilder();
             if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) {
                 stringBuilder.append("public ");
-            }
-            if ((method.getModifiers() & Modifier.STATIC) == Modifier.STATIC) {
-                stringBuilder.append("static ");
-            }
 
-            stringBuilder.append(method.getGenericReturnType()).append(" ");
-            stringBuilder.append(method.getName());
-            stringBuilder.append(Arrays.toString(method.getParameterTypes()));
-            String output = stringBuilder.toString().replaceAll("]",")").replace("[","(");
-            System.out.println(output);
+                if ((method.getModifiers() & Modifier.STATIC) == Modifier.STATIC) {
+                    stringBuilder.append("static ");
+                }
+
+                stringBuilder.append(method.getGenericReturnType()).append(" ");
+                stringBuilder.append(method.getName());
+                stringBuilder.append(Arrays.toString(method.getParameterTypes()));
+                String output = stringBuilder.toString().replaceAll("]", ")")
+                        .replace("[", "(")
+                        .replaceAll(", ", ",");
+                System.out.println(output);
+            }
+        }
+        Field[] fields = structMath.getDeclaredFields();
+        for (Field field : fields) {
+            if ((field.getModifiers()& Modifier.PUBLIC)==Modifier.PUBLIC){
+                StringBuilder stringBuilder = new StringBuilder();
+                if ((field.getModifiers() & Modifier.STATIC)==Modifier.STATIC){
+                    stringBuilder.append(field.getGenericType()).append(" ").append(field.getName());
+                }
+                System.out.println(stringBuilder);
+            }
         }
     }
 }
