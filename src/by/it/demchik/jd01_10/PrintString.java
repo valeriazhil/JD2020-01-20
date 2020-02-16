b09256598ebd5588511358bf0a3542d5abac8d29
vadimdemchik <demchikvadimm@gmail.com>
2020-02-16 15:50:48 +0300

    comm 10

--- src/by/it/demchik/jd01_10/PrintString.java
+++ src/by/it/demchik/jd01_10/PrintString.java
@@ -5,9 +5,12 @@ import java.lang.reflect.Modifier;
 
 public class PrintString {
     public static void main(String[] args) {
-        Class<String> sys=String.class;
+        Class<String> sys = String.class;
+        Method[] methods = sys.getDeclaredMethods();
+        for (Method method : methods) {
+            if ((method.getModifiers() & Modifier.STATIC) != Modifier.STATIC)
+                System.out.println(method.getName());
+        }
 
 
-        System.out.println(method.toString().replace("java.lang.String.",""));
     }}
-}
