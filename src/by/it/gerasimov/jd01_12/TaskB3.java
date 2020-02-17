7869f0abd566034f540327bd542f436f94dd9326
Gerasimov Jegor <herasimau.jahor@gmail.com>
2020-02-17 13:38:04 +0300

    fix jd01_12 B3

--- src/by/it/gerasimov/jd01_12/TaskB3.java
+++ src/by/it/gerasimov/jd01_12/TaskB3.java
@@ -18,7 +18,7 @@ public class TaskB3 {
         return people.get(0);
     }
 
-    static <T> T process(LinkedList<T> people) {
+    static <T> T processAsArrayList(LinkedList<T> people) {
         Iterator<T> it = people.iterator();
         while (people.size() > 1) {
             for (int i = 0; i < 2; i++) {
@@ -32,6 +32,14 @@ public class TaskB3 {
         return people.get(0);
     }
 
+    static <T> T process(LinkedList<T> people) {
+        while (people.size() > 1) {
+            people.addLast(people.pollFirst());
+            people.pollFirst();
+        }
+        return people.peekFirst();
+    }
+
     public static void main(String[] args) {
         String[] strPeople = new String[4096];
         for (int i = 0; i < strPeople.length; i++) {
@@ -42,6 +50,9 @@ public class TaskB3 {
         LinkedList<String> linkedPeople = new LinkedList<>(people);
         double start = System.nanoTime();
         System.out.println(process(arrayPeople));
+        System.out.println((System.nanoTime() - start) / 1000);
+        start = System.nanoTime();
+        System.out.println(processAsArrayList(linkedPeople));
         System.out.println((System.nanoTime() - start) / 1000);
         start = System.nanoTime();
         System.out.println(process(linkedPeople));
