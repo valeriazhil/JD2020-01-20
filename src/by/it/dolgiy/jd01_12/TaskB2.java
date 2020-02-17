28d6b0c956d15c862c94348328db814b13b66410
EvgeniyD1 <edolgiyj7@gmail.com>
2020-02-17 23:55:17 +0300

    11

--- src/by/it/dolgiy/jd01_12/TaskB2.java
+++ src/by/it/dolgiy/jd01_12/TaskB2.java
@@ -9,46 +9,37 @@ class TaskB2 {
 
     static String process(ArrayList<String> peoples){
         ArrayList<String> list1 = new ArrayList<>(peoples);
-        Iterator<String> iterator = list1.iterator();
         System.out.println(list1);
-        while (iterator.hasNext()){
-            while (list1.size() > (list1.size()/2) && list1.size()!=1){
-                int a = list1.size()/2;
-                for (int i = 0; i < a; i++) {
-                    iterator.next();
-                    iterator.remove();
-                }
-                System.out.println(list1);
+        while (list1.size()!=1){
+            Iterator<String> iterator = list1.iterator();
+            while (iterator.hasNext()) {
+                iterator.next();
+                iterator.next();
+                iterator.remove();
             }
-            if (list1.size()==1) {
-                break;
-            }
+            System.out.println(list1);
         }
         return String.valueOf(list1);
     }
 
     static String process(LinkedList<String> peoples){
         LinkedList<String> list2 = new LinkedList<>(peoples);
-        Iterator<String> iterator = list2.iterator();
         System.out.println(list2);
-        while (iterator.hasNext()){
-            while (list2.size() > (list2.size()/2) && list2.size()!=1){
-                int a = list2.size()/2;
-                for (int i = 0; i < a; i++) {
-                    iterator.next();
-                    iterator.remove();
-                }
-                System.out.println(list2);
+        while (list2.size()!=1){
+            Iterator<String> iterator = list2.iterator();
+            while (iterator.hasNext()) {
+                iterator.next();
+                iterator.next();
+                iterator.remove();
             }
-            if (list2.size()==1) {
-                break;
-            }
+            System.out.println(list2);
         }
         return String.valueOf(list2);
     }
 
     public static void main(String[] args) {
         process((ArrayList<String>) list1);
+        System.out.println();
         process((LinkedList<String>) list2);
     }
 }
