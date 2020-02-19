b0ceff356759198e695213403ba4094adf191d26
Scaryshki <Scaryshki@gmail.com>
2020-02-19 11:18:53 +0300

    task 12 13

--- src/by/it/makarenko/jd01_04/TaskB.java
+++ src/by/it/makarenko/jd01_04/TaskB.java
@@ -10,19 +10,28 @@ public class TaskB {
         String famil = sc.next();
         String famil1 = sc.next();
         String famil2 = sc.next();
-        String [] arrayFam = {famil,famil1,famil2};
-        for (int i = 0; i <arrayFam.length ; i++) {
+        String[] arrayFam = {famil, famil1, famil2};
+        for (int i = 0; i < arrayFam.length; i++) {
             System.out.println(arrayFam[i]);
         }
-        int x=4;
-        int y=4;
-        int [][] cash = new int[x][4];
-        for (int i = 0; i <x ; i++) {
-            cash[i]=new int[x];
-            for (int j = 0; j <y ; j++) {
-                cash[i][j] = sc.nextInt();
-            }
-        }
-        System.out.println(cash[x][y]);
+        System.out.println("Введите зарплату для " + famil);
+        int zarPlat = sc.nextInt();
+//      int[] arrFamil = new int[zarPlat];
+//      for (int i = 0; i < arrayFam.length; i++) {
+//          arrFamil[i]=arrFamil[i];
+//      }
+
+        System.out.println("Введите зарплату для " + famil1);
+        int zarPlat1 = sc.nextInt();
+//        int[] arrFamil1 = new int[zarPlat1];
+//        for (int i = 0; i < arrFamil1.length; i++) {
+//        arrFamil1[i]=arrFamil1[i];
+//        }
+        System.out.println("Введите зарплату для " + famil2);
+        int zarPlat2 = sc.nextInt();
+//        int[] arrFamil2 = new int[zarPlat2];
+//        for (int i = 0; i < arrFamil2.length; i++) {
+//        arrFamil2[i]=arrFamil2[i];
+//        }
     }
 }
