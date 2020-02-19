4e30fc1a85a2d21f122d1eb25e70d915f7597203
EvgeniyD1 <edolgiyj7@gmail.com>
2020-02-19 23:44:57 +0300

    11

--- src/by/it/dolgiy/jd01_13/TaskC.java
+++ src/by/it/dolgiy/jd01_13/TaskC.java
@@ -5,34 +5,31 @@ import java.util.Scanner;
 
 class TaskC {
 
-//    static void readData(){
-//        ArrayList<Double> values = new ArrayList<>();
-//        for (Double value : values) {
-//            value = ;
-//        }
-//    }
+        static void readData(Scanner scanner) throws InterruptedException {
+        ArrayList<Double> values = new ArrayList<>();
+            int counter = 0;
+            while (true){
+                try {
+                    String text = scanner.next();
+                    double value = Double.parseDouble(text);
+                    values.add(0,value);
+                }
+                catch (Exception e){
+                    Thread.sleep(100);
+                    for (Double value1 : values) {
+                        System.out.print(value1);
+                    }
+                    counter++;
+                }
+                if (counter==5){
+                    System.out.println();
+                    throw new InterruptedException();
+                }
+            }
+    }
 
     public static void main(String[] args) throws InterruptedException {
-        ArrayList<Double> values = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        int counter = 0;
-        while (true){
-            try {
-                String text = scanner.next();
-                double value = Double.parseDouble(text);
-                values.add(0,value);
-            }
-            catch (Exception e){
-                Thread.sleep(100);
-                for (Double value1 : values) {
-                    System.out.print(value1);
-                }
-                counter++;
-            }
-            if (counter==5){
-                System.out.println();
-                throw new InterruptedException();
-            }
-        }
+        readData(scanner);
     }
 }
