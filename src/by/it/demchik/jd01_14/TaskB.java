7218c4ab2b1769803aebc9b6f7f055a506682150
vadimdemchik <demchikvadimm@gmail.com>
2020-02-23 21:45:39 +0300

    comm 14

--- src/by/it/demchik/jd01_14/TaskB.java
+++ src/by/it/demchik/jd01_14/TaskB.java
@@ -6,28 +6,37 @@ public class TaskB {
     public static void main(String[] args) {
         String path = System.getProperty("user.dir") + File.separator + "src/by/it/demchik/jd01_14/";
 
-
         String filename = path + "text.txt";
         File f = new File(filename);
-        int b, count = 0;
+        int counter=0;
         FileReader is = null;
-        try {
+        try{
             is = new FileReader(f);
-            while ((b = is.read()) != -1) {
-                count++;
-            }
-            System.out.println("число слов= " + count);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (is != null) {
-                    is.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
-    }
-}
+            int s;
+            // int a=0;
+            while((s = is.read()) != -1) {
+
+                char ch = (char) s;
+                if(ch=='!')counter++;
+                if (ch==' ')counter++;
+                if (ch==',')counter++;
+                if(ch=='.')counter++;
+//if (ch = " ") a++;
+//System.out.print(a);
+                System.out.print("");}
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
+        catch(IOException e) {
+            System.out.println("IO exception: " + e.getMessage());
+        }
+        finally {if (is != null) {
+            try {is.close();
+            }
+            catch(IOException e) {
+                System.out.println("Неверная ссылка к файлу");
+            }
+        }}
+        System.out.println(""+"\n Колличество слов - "+counter);}}
