078e862bad493be73c0ae83081dcfa4fe54b46ec
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-02-23 10:25:23 +0300

    15 ABC

--- src/by/it/zhuravaskarabahataya/jd01_15/TaskC.java
+++ src/by/it/zhuravaskarabahataya/jd01_15/TaskC.java
@@ -9,62 +9,102 @@ package by.it.zhuravaskarabahataya.jd01_15;
 //  Стартовым каталогом при запуске приложения должна быть папка by.it.ваша_фамилия.jd01_15.
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.*;
 import java.util.Scanner;
 
 class TaskC {
 
     private static String startingPoint = Helper.getPath("TaskC.java", TaskC.class);
-    private static Path myPoint = Paths.get(startingPoint);
+    private static Path myPoint = Paths.get(startingPoint).getParent();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(startingPoint);
+        System.out.println("Вы находитесь здесь:" + myPoint);
         String s;
-        Path p1 = Paths.get(startingPoint + File.separator);
-        Path p2 = p1.getParent();
-
-        System.out.println(myPoint);
-        Files files;
-        FileSystem fs = FileSystems.getDefault();
-        System.out.println(fs);
         while (true) {
             s = sc.nextLine();
-            if (s != null && s.equals("end")) {
+            if (s == null || s.equals("end")) {
                 break;
             } else {
-                procees(s);
+                process(s);
             }
         }
-
     }
 
-    private static void procees(String s) {
+    private static void process(String s) {
         switch (s) {
             case "cd ..": {
                 System.out.println("Вы вышли из папки");
                 myPoint = myPoint.getParent();
+                startingPoint = myPoint.toString();
                 System.out.println(myPoint);
                 break;
             }
             case "dir": {
-                System.out.println("Список файлов:");
+                printDir();
                 break;
             }
             case "end": {
                 break;
             }
             default: {
-                if (s.matches("cd [a-zA-Zа-яА-ЯёЁ0-9]+")){
-                    String newFolder = s
-                            .replace(" ","")
-                            .substring(2, s.length() -1);
-                    System.out.println("Выбран католог " + newFolder);
+                if (s.matches("cd [-_a-zA-Zа-яА-ЯёЁ0-9]+")) {
+                    chooseCatalog(s);
+                } else {
+                    System.out.println("Такой команды не найдено:(");
+                    break;
                 }
-                else{
-                System.out.println("Такой команды не найдено:(");
-                break;}
             }
+        }
+    }
+
+    private static void printDir (){
+        File file = new File(myPoint.toString());
+        if (file.isDirectory()) {
+            System.out.println("Это папка " + file);
+            System.out.println("Список файлов:");
+
+        } else {
+            System.out.println("Это не папка");
+        }
+        File[] files = file.listFiles();
+        getFiles(files);
+    }
+
+    private static void chooseCatalog(String s) {
+        String newFolder = s
+                .replace(" ", "")
+                .substring(2, s.length() - 1);
+        System.out.println("Выбран католог " + newFolder);
+        startingPoint = startingPoint + File.separator + newFolder;
+        myPoint = Paths.get(startingPoint);
+        System.out.println(myPoint);
+    }
+
+    private static void getFiles(File[] directory) {
+        try {
+            for (File file : directory) {
+                if (file.isDirectory()) {
+                    String path = file.getPath();
+                    Object date = Files.getAttribute(Paths.get(path), "basic:creationTime");
+                    String d = date.toString();
+                    String di = "<DIR>";
+                    System.out.printf("%-28s %9s %-20s\n", d, di, file.getName());
+                    getFiles(file.listFiles());
+                } else {
+                    String path = file.getPath();
+                    Object date = Files.getAttribute(Paths.get(path), "basic:creationTime");
+                    String d = date.toString();
+
+                    System.out.printf("%-28s           %-20s\n", d, file.getName());
+
+                }
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Список пуст");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
