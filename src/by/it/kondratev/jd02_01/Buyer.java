f4c21dc15db6d692b3de7656e560916a3b7e6328
Михаил <potapy4@tut.by>
2020-02-29 15:18:32 +0300

    2902

--- src/by/it/kondratev/jd02_01/Buyer.java
+++ src/by/it/kondratev/jd02_01/Buyer.java
@@ -1,27 +1,49 @@
 package by.it.kondratev.jd02_01;
 
-class Buyer extends Thread implements IBuyer{
+import java.util.List;
 
-    Buyer (int number) {
-        super("Покупатель № " + number);
-    }
+class Buyer extends Thread implements IBuyer, IuseBacket {
+
+
+    Backet backet = new Backet();
+    List<String> list = backet.list;
+
+    Buyer (int number) { super( "Покупатель № " + number); }
 
     public void run() {
         enterToMarket();
+        takeBacket();
         chooseGoods();
         goOut();
     }
 
     @Override
     public void enterToMarket() {
-        System.out.println("Зашёл в магазин " + this.getName());
+        System.out.println("Зашёл в магазин " + getName());
+    }
+
+    @Override
+    public void takeBacket() {
+        System.out.println("Взял корзину " + getName());
     }
 
     @Override
     public void chooseGoods()  {
-        System.out.println("Начал выбирать товары " + this.getName());
-        Helper.sleep(Helper.random(500,2000));
-        System.out.println("Окончил выбирать товары " + this.getName());
+        System.out.println("Начал выбирать товары " + getName());
+        int goods_count = Helper.random(1,4);
+        for (int i = 0; i < goods_count; i++) {
+            putGoodsToBacket();
+        }
+        System.out.println("Окончил выбирать товары " + getName());
+
+    }
+
+    @Override
+    public void putGoodsToBacket() {
+        int good_number = Helper.random(0,Market.goods_list.size()-1);
+        String good_name = Market.goods_list.get(good_number);
+        list.add(good_name);
+        System.out.println("Положил " + good_name + " в корзину " + getName());
     }
 
     @Override
