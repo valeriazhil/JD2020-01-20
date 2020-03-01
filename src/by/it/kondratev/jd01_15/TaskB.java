f7b9dd8cbf66c13c921ccd054c8e909a13a94c85
Михаил <potapy4@tut.by>
2020-03-01 20:46:02 +0300

    2902

--- src/by/it/kondratev/jd01_15/TaskB.java
+++ src/by/it/kondratev/jd01_15/TaskB.java
@@ -1,4 +1,34 @@
 package by.it.kondratev.jd01_15;
 
+import java.io.*;
+
 public class TaskB {
+    public static void main(String[] args) {
+        read_from_java();
+
+    }
+
+    private static StringBuilder read_from_java() {
+        String java_name = getPath() + TaskB.class.getSimpleName() + ".java";
+        StringBuilder result = new StringBuilder();
+
+
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(java_name)
+        )) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+        static String getPath () {
+            String root = System.getProperty("user.dir");
+            String name = TaskB.class.getName();
+            name = name.replace('.', File.separatorChar).
+                    replace(TaskB.class.getSimpleName(), "");
+            return root + File.separatorChar + "src" + File.separatorChar + name; // + "matrix.txt";
+        }
+
 }
