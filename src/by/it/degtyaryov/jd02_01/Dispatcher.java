eeeb7d06a36a052db2cf7de11d9468725c14f420
Degtyaryov Eugene <EDegtyaryov@qulix.com>
2020-03-02 18:29:13 +0300

    Add synchronization in jd02_01

--- src/by/it/degtyaryov/jd02_01/Dispatcher.java
+++ src/by/it/degtyaryov/jd02_01/Dispatcher.java
@@ -2,5 +2,42 @@ package by.it.degtyaryov.jd02_01;
 
 class Dispatcher {
 
-    public static int buyerInMarket = 0;
+    private static final int BUYERS_BEGIN_MINUTE = 10;
+    private static final int BUYERS_MIDDLE_MINUTE = 40;
+
+    private static int buyersCounter;
+    private static int buyersInMarket;
+
+    public static synchronized void newBuyerInMarket() {
+		buyersCounter++;
+        buyersInMarket++;
+    }
+
+    public static synchronized void buyerLeaveMarket() {
+        buyersInMarket--;
+    }
+
+	public static int getBuyersCounter() {
+        return buyersCounter;
+    }
+	
+    public static int getBuyersInMarket() {
+        return buyersInMarket;
+    }
+
+    public static int getBuyerToEnterByTime(int time) {
+        int mustBeInMarket = Dispatcher.getCountMustBeInMarket(time);
+        return (Dispatcher.getBuyersInMarket() > mustBeInMarket) ?
+                0 : mustBeInMarket - Dispatcher.getBuyersInMarket();
+    }
+
+    private static int getCountMustBeInMarket(int time) {
+        int count;
+        time %= 60; // для привязки к секунде без учета минуты
+        if (time < 30)
+            count = BUYERS_BEGIN_MINUTE + time;
+        else
+            count = BUYERS_MIDDLE_MINUTE + (30 - time);
+        return count;
+    }
 }
