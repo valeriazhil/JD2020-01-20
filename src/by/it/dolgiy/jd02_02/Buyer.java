9f3d3c7cc5e5a9edc52c9af06a2717366f759f50
EvgeniyD1 <edolgiyj7@gmail.com>
2020-03-03 20:30:12 +0300

    JD02_02

---
+++ src/by/it/dolgiy/jd02_02/Buyer.java
@@ -0,0 +1,92 @@
+package by.it.dolgiy.jd02_02;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+class Buyer extends Thread implements IBuyer, IUseBasket {
+
+    Buyer(int number,String buyer){
+        super("Buyer № " + number + buyer);
+    }
+
+    @Override
+    public void run() {
+        enterToMarket();
+        takeBasket();
+        chooseGoods();
+        putGoodsToBasket();
+        goToQueue();
+        goOut();
+    }
+
+    @Override
+    public void enterToMarket() {
+        System.out.println(this+" enter to the Market");
+        Dispatcher.newBuyer();
+    }
+
+    @Override
+    public void takeBasket() {
+        System.out.println(this+" took a basket");
+    }
+
+    @Override
+    public void chooseGoods() {
+        System.out.println(this+" started choosing goods");
+        int timeout = Helper.random(500,2000);
+        if (Runner.pensioner){
+            timeout = (int) (timeout*1.5);
+        }
+        Helper.sleep(timeout);
+        System.out.println(this+" finished choosing goods");
+    }
+
+    @Override
+    public void putGoodsToBasket() {
+        System.out.println(this + " put products to basket");
+//        Set<Map.Entry<String, Integer>> basket = Helper.shoppingList.entrySet();
+//        Iterator<Map.Entry<String, Integer>> iterator = basket.iterator();
+//        StringBuffer shopL = new StringBuffer(this+" basket :: ");
+//        int countProducts = Helper.random(1,4);
+//        for (int i = 0; i < countProducts; i++) {
+//            int timeout = Helper.random(500,2000);
+//            if (Runner.pensioner){
+//                timeout = (int) (timeout*1.5);
+//            }
+//            Helper.sleep(timeout);
+//            /////////////////
+//            int choice = Helper.random(1);
+//            for (int ch = 0; ch < choice; ch++) {
+//                iterator.next();
+//            }
+//            Map.Entry<String, Integer> next = iterator.next();
+//            shopL.append(next.getKey()).append(':').append(next.getValue()).append(';');
+//        }
+//        System.out.println(shopL);
+    }
+
+    @Override
+    public void goToQueue() {
+        System.out.println(this+" queued");
+        synchronized (this){
+            QueueBuyers.add(this);
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                System.out.println("Error:"+e);
+            }
+        }
+    }
+
+    @Override
+    public void goOut() {
+        System.out.println(this+" leave the Market");
+        Dispatcher.buyerLeaveTheMarket();
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+}
---
+++ src/by/it/dolgiy/jd02_02/Runner.java
@@ -0,0 +1,87 @@
+package by.it.dolgiy.jd02_02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Runner {
+
+    static boolean pensioner;
+    static String buyerStr;
+
+    public static void main(String[] args) {
+        System.out.println("The market opened");
+        List<Thread> threads = new ArrayList<>();
+
+//        for (int i = 1; i <= 2; i++) {
+//            Cashier cashier = new Cashier(i);
+//            Thread thread = new Thread(cashier);
+//            threads.add(thread);
+//            thread.start();
+//        }
+
+        while (Dispatcher.marketIsOpened()){
+//            for (int minute = 0; minute < 2; minute++) {
+//                for (int second = 0; second < 60; second++) {
+            int currentCount = Helper.random(2);
+            for (int count = 0; count < currentCount && Dispatcher.marketIsOpened(); count++) {
+                buyerStr = typeOfBuyer();
+                int number = Dispatcher.numberBuyer;
+                Buyer buyer = new Buyer(number,buyerStr);
+                buyer.start();
+//                        buyerInMarket(second, buyer);
+            }
+            Helper.sleep(1000);
+            openCash(threads);
+//                }
+//            }
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Error:"+e);
+            }
+        }
+
+        System.out.println("The market closed");
+    }
+
+    private static void openCash(List<Thread> threads) {
+        if (QueueBuyers.QUEUE.size()>threads.size()*5 && threads.size()<5) {
+            Cashier cashier = new Cashier(threads.size() + 1);
+            Thread thread = new Thread(cashier);
+            threads.add(thread);
+            thread.start();
+        }
+    }
+
+//    private static void buyerInMarket(int time, Buyer buyer) {
+//        if (Dispatcher.buyersInMarket>=40-(30-time)){
+//            try {
+//                buyer.join();
+//            } catch (InterruptedException e) {
+//                System.out.println("Error:"+e);
+//            }
+//        }
+//    }
+
+    private static String typeOfBuyer() {
+        String buyerStr;
+        if (Dispatcher.numberBuyer%4==0){
+            pensioner= Helper.random();
+            if (pensioner){
+                buyerStr = " pensioner";
+            }
+            else{
+                buyerStr = "";
+            }
+        }
+        else {
+            pensioner=false;
+            buyerStr = "";
+        }
+        return buyerStr;
+    }
+
+}
