df835857b71f85209e34cb1dd99326d6a250531a
Михаил <potapy4@tut.by>
2020-03-03 13:31:54 +0300

    01/03

--- src/by/it/kondratev/Proghub/Search.java
+++
@@ -1,8 +0,0 @@
-package by.it.kondratev.Proghub;
-
-public class Search {
-    public static void main(String[] args) {
-        String topicName = Help.topic();
-        Help.search(topicName);
-    }
-}
