c1dd2f7a38b666d25b2ef346e176b1d41687808f
zhilenkova <valeriz.zhil@gmail.com>
2020-03-08 19:40:53 +0300

    1

---
+++ src/by/it/zhilenkova/jd02/jd02_04/CalcException.java
@@ -0,0 +1,19 @@
+package by.it.zhilenkova.jd02.jd02_04;
+
+public class CalcException extends Exception {
+
+    public CalcException() {
+    }
+
+    public CalcException(String message) {
+        super("Error: "+message);
+    }
+
+    public CalcException(String message, Throwable why) {
+        super("Error: "+message, why);
+    }
+
+    public CalcException(Throwable why) {
+        super(why);
+    }
+}
