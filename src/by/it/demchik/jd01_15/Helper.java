4b8dae0ee118d06fd74c7348b3225b7d0303e856
vadimdemchik <demchikvadimm@gmail.com>
2020-03-08 21:30:08 +0300

    comm

--- src/by/it/demchik/jd01_15/Helper.java
+++ src/by/it/demchik/jd01_15/Helper.java
@@ -3,15 +3,13 @@ package by.it.demchik.jd01_15;
 import java.io.File;
 
 class Helper {
-
     private static final String root = System.getProperty("user.dir");
-
-    static String getPath(Class<?> aClass, String fileName) {
-        String name = aClass.getName();
-        String path=name
-                .replace(aClass.getSimpleName(),"")
-                .replace(".", File.separator);
-        return root+File.separator+"src"+File.separator+path+fileName;
+    static String getPath(String fileName, Class<?> cl) {
+        String name = cl.getName();
+        String path = name
+                .replace(cl.getSimpleName(), "")
+                .replace('.', File.separatorChar);
+        path = root + File.separator + "src" + File.separator + path +  fileName;
+        return path;
     }
-
 }
---
+++ src/by/it/demchik/jd02_02/IBuyer.java
@@ -0,0 +1,12 @@
+package by.it.demchik.jd02_02;
+
+interface IBuyer {
+
+    void enterToMarket(); //вошел в магазин (мгновенно)
+
+    void chooseGoods(); //выбрал товар (от 0,5 до 2 секунд)
+
+    void goToQueue(); //в очередь
+
+    void goOut(); //отправился на выход(мгновенно)
+}
