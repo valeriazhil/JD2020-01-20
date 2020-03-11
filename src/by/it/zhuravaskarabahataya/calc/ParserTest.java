819625b9138b889412bda1c2f0d74e61140857ff
Anastasiya Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-03-11 09:28:29 +0300

    jd02_05 completed

--- src/by/it/zhuravaskarabahataya/calc/ParserTest.java
+++ src/by/it/zhuravaskarabahataya/calc/ParserTest.java
@@ -2,8 +2,6 @@ package by.it.zhuravaskarabahataya.calc;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.junit.Assert.*;
 
 public class ParserTest {
@@ -32,7 +30,7 @@ public class ParserTest {
     public void calcScalarMulScalar() throws CalcException {
         double expected = 25.55;
         Parser parser = new Parser();
-        String expression = "B=A*3.5";
+        String expression = "B=7.3*3.5";
         Var var = parser.calc(expression);
         double actual = Double.parseDouble(var.toString());
         assertEquals(expected, actual, 1e-5);
@@ -48,11 +46,11 @@ public class ParserTest {
         assertEquals(expected, actual, 1e-5);
     }
 
-    @Test (expected = CalcException.class)
+    @Test(expected = CalcException.class)
     public void calcScalarDivNull() throws CalcException {
         Parser parser = new Parser();
         String expression = "27/0";
-        Var var = parser.calc(expression);
+        parser.calc(expression);
     }
 
     @Test
@@ -69,7 +67,7 @@ public class ParserTest {
     public void calcA4() throws CalcException {
         double expected = 2.65;
         Parser parser = new Parser();
-        String expression = "B2=A/2-1";
+        String expression = "B2=7.3/2-1";
         Var var = parser.calc(expression);
         double actual = Double.parseDouble(var.toString());
         assertEquals(expected, actual, 1e-5);
@@ -105,11 +103,11 @@ public class ParserTest {
         assertEquals("Incorrect scalar mul vector ", expected, actual);
     }
 
-    @Test (expected = CalcException.class)
+    @Test(expected = CalcException.class)
     public void calcScalarDivVector() throws CalcException {
         Parser parser = new Parser();
         String expression = "7/{1, 2}";
-        Var var = parser.calc(expression);
+        parser.calc(expression);
     }
 
 
@@ -143,11 +141,11 @@ public class ParserTest {
         assertEquals("Incorrect scalar mul matrix ", expected, actual);
     }
 
-    @Test (expected = CalcException.class)
+    @Test(expected = CalcException.class)
     public void calcScalarDivMatrix() throws CalcException {
         Parser parser = new Parser();
         String expression = "7/{{2, 3}, {1, 2}}";
-        Var var = parser.calc(expression);
+        parser.calc(expression);
     }
 
     @Test
@@ -180,15 +178,52 @@ public class ParserTest {
         assertEquals("Incorrect vector mul vector ", expected, actual);
     }
 
-    @Test (expected = CalcException.class)
+    @Test(expected = CalcException.class)
     public void calcVectorDivVector() throws CalcException {
         Parser parser = new Parser();
         String expression = "{1,2,3}/{2,3,4}";
-        Var var = parser.calc(expression);
+        parser.calc(expression);
     }
 
     @Test
-    public void testVarCreation() throws CalcException{
+    public void calcMatrixAddMatrix() throws CalcException {
+        String expected = "{{3.0, 6.0}, {7.0, 0.0}}";
+        Parser parser = new Parser();
+        String expression = "{{2, 4} , {0, 0}} + {{1, 2},{7, 0}}";
+        Var var = parser.calc(expression);
+        String actual = var.toString();
+        assertEquals("Incorrect matrix add matrix", expected, actual);
+    }
+
+    @Test
+    public void calcMatrixSubMatrix() throws CalcException {
+        String expected = "{{1.0, 2.0}, {-7.0, 0.0}}";
+        Parser parser = new Parser();
+        String expression = "{{2, 4} , {0, 0}} - {{1, 2},{7, 0}}";
+        Var var = parser.calc(expression);
+        String actual = var.toString();
+        assertEquals("Incorrect matrix seb matrix ", expected, actual);
+    }
+
+    @Test
+    public void calcMatrixMulMatrix() throws CalcException {
+        String expected = "{{30.0, 4.0}, {0.0, 0.0}}";
+        Parser parser = new Parser();
+        String expression = "{{2, 4} , {0, 0}} * {{1, 2},{7, 0}}";
+        Var var = parser.calc(expression);
+        String actual = var.toString();
+        assertEquals("Incorrect matrix mul matrix ", expected, actual);
+    }
+
+    @Test(expected = CalcException.class)
+    public void calcMatrixDivMatrix() throws CalcException {
+        Parser parser = new Parser();
+        String expression = "{{2, 4} , {0, 0}} / {{1, 2},{7, 0}}";
+        parser.calc(expression);
+    }
+
+    @Test
+    public void testVarCreation() throws CalcException {
         String expectedScalar = "2.0";
         String expectedVector = "{1.0, 2.0, 5.0}";
         String expectedMatrix = "{{2.0, 3.0, 77.0}, {6.0, 2.0, 9.0}}";
@@ -196,9 +231,9 @@ public class ParserTest {
         String expressionScalar = " A =2";
         String expressionVector = " B ={1, 2, 5}";
         String expressionMatrix = " M =  {{2.0, 3.0, 77.0}, {6.0, 2.0, 9.0}}";
-        Var resultScalar = parser.calc(expectedScalar);
-        Var resultVector = parser.calc(expectedVector);
-        Var resultMatrix = parser.calc(expectedMatrix);
+        Var resultScalar = parser.calc(expressionScalar);
+        Var resultVector = parser.calc(expressionVector);
+        Var resultMatrix = parser.calc(expressionMatrix);
         assertEquals("Var scalar creation invalid ", expectedScalar, resultScalar.toString());
         assertEquals("Var vector creation invalid ", expectedVector, resultVector.toString());
         assertEquals("Var vector creation invalid ", expectedMatrix, resultMatrix.toString());
--- src/by/it/zhuravaskarabahataya/jd02_05/ResMan.java
+++ src/by/it/zhuravaskarabahataya/jd02_05/ResMan.java
@@ -6,15 +6,14 @@ import java.util.ResourceBundle;
 public enum ResMan {
     INSTANCE;
 
-    private String baseName = "by.it.zhuravaskarabahataya.jd02_05.translate.lang";
-
     ResourceBundle bundle;
 
-    private ResMan() {
+    ResMan() {
         setLocale(Locale.getDefault());
     }
 
     void setLocale(Locale locale){
+        String baseName = "by.it.zhuravaskarabahataya.jd02_05.translate.lang";
         bundle = ResourceBundle.getBundle(baseName, locale);
     }
 
