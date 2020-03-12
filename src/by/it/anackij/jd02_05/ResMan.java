82c8ac27972229cbd2a79b58aa21dd68136c87e6
naglec13 <naglec13@gmail.com>
2020-03-12 23:04:15 +0300

    ..jd02_05 - rewrite

--- src/by/it/anackij/jd02_05/ResMan.java
+++ src/by/it/anackij/jd02_05/ResMan.java
@@ -1,4 +1,24 @@
 package by.it.anackij.jd02_05;
 
-public class ResMan {
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+enum ResMan {
+    INSTANCE;
+
+    private String locationRes = "by.it.anackij.jd02_05.properties.Messages";
+
+    private ResourceBundle resourceBundle;
+
+    ResMan() {
+        setLocale(Locale.ENGLISH);
+    }
+
+    void setLocale(Locale locale) {
+        resourceBundle = ResourceBundle.getBundle(locationRes, locale);
+    }
+
+    String get(String key) {
+        return resourceBundle.getString(key);
+    }
 }
--- src/by/it/anackij/jd02_05/Runner.java
+++ src/by/it/anackij/jd02_05/Runner.java
@@ -1,4 +1,47 @@
 package by.it.anackij.jd02_05;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Runner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Locale locale = new Locale(System.getProperty("user.language"), System.getProperty("user.country"));
+        ResMan res = ResMan.INSTANCE;
+        res.setLocale(locale);
+
+        if (args.length == 2) {
+            locale = new Locale(args[0], args[1]);
+            res.setLocale(locale);
+        }
+        do {
+            System.out.println(res.get(Messages.FIRSTNAME) + " " + res.get(Messages.LASTNAME));
+            System.out.println(res.get(Messages.WELCOME));
+            System.out.println(res.get(Messages.QUESTION));
+            System.out.print(res.get(Messages.CURRENT_DATE) + " ");
+            Calendar calendar = Calendar.getInstance(locale);
+            DateFormat dt = DateFormat.getDateInstance(DateFormat.LONG, locale);
+            String longLocaleDate = dt.format(calendar.getTime());
+            System.out.println(longLocaleDate);
+            System.out.println(res.get(Messages.ENTER_ACTION));
+
+            String expression = scanner.next();
+            if (expression.toLowerCase().equals("end")) break;
+            else if (expression.toLowerCase().equals("en")) {
+                locale = new Locale("en", "US");
+                res.setLocale(locale);
+                //res.setLocale(Locale.ENGLISH);
+            } else if (expression.toLowerCase().equals("by") || expression.toLowerCase().equals("be")) {
+                locale = new Locale("be", "BY");
+                res.setLocale(locale);
+            } else if (expression.toLowerCase().equals("ru")) {
+                locale = new Locale("ru", "RU");
+                res.setLocale(locale);
+            }
+        } while (true);
+    }
+
 }
