77d92b9889f4ca5070a23589f25ce00d3d04b797
zhilenkova <valeriz.zhil@gmail.com>
2020-03-12 21:19:54 +0300

    1

--- src/by/it/zhilenkova/jd02/jd02_06/ZhilenkovaCalculator/Patterns.java
+++
@@ -1,8 +0,0 @@
-package by.it.zhilenkova.jd02.jd02_06.ZhilenkovaCalculator;
-
-final class Patterns {
-    static final String OPERATION="(?<![-+*/={,])([-+=*/])";
-    static final String SCALAR = "-?\\d+(.\\d+)?";
-    static final String VECTOR = "\\{((-?\\d+(.\\d+)?)(,?))+}";
-    static final String MATRIX = "\\{(\\{((-?\\d+(.\\d+)?)(,?))+},?)+}";
-}
--- src/by/it/zhilenkova/jd02/jd02_06/ZhilenkovaCalculator/Printer.java
+++
@@ -1,7 +0,0 @@
-package by.it.zhilenkova.jd02.jd02_06.ZhilenkovaCalculator;
-
-public class Printer {
-    void print(String var){
-        System.out.println(var);
-    }
-}
