240404b3f5c409bc592ec7ffa830698c0262e078
Михаил <potapy4@tut.by>
2020-03-12 22:23:39 +0300

    12/03

---
+++ src/by/it/kondratev/jd02_05/Runner.java
@@ -0,0 +1,29 @@
+package by.it.kondratev.jd02_05;
+
+import by.it.kondratev.jd02_05.ResMan;
+import by.it.kondratev.jd02_05.translate.Message;
+import by.it.kondratev.jd02_05.translate.User;
+
+import java.util.Locale;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        ResMan manager=ResMan.INSTANCE;
+        Locale locale = Locale.getDefault();
+        if (args.length == 2) {
+            String lang = args[0];
+            String country = args[1];
+            locale = new Locale(lang, country);
+        }
+        manager.setLocale(locale);
+        
+        
+        System.out.println(manager.get(Message.WELCOME));
+        System.out.println(manager.get(Message.QUESTION));
+        System.out.println(manager.get(User.FIRST_NAME));
+        System.out.println(manager.get(User.LAST_NAME));
+    }
+
+
+}
---
+++ src/by/it/kondratev/jd02_05/translate/Message.java
@@ -0,0 +1,7 @@
+package by.it.kondratev.jd02_05.translate;
+
+public interface Message {
+
+    String WELCOME = "message.welcome";
+    String QUESTION = "message.question";
+}
